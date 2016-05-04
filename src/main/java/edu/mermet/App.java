65af5f3113c021efb9f0afb8bb488bca0820f16a
Bruno Mermet <Bruno.Mermet@univ-lehavre.fr>
2016-05-04 14:44:24 +0200

    Ajout de checkstyle

--- src/main/java/edu/mermet/App.java
+++ src/main/java/edu/mermet/App.java
@@ -3,11 +3,16 @@ package edu.mermet;
 /**
  * Hello world!
  *
+ * @author mermet
+ * @version $Id: $Id
  */
-public class App 
-{
-    public static void main( String... args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    /**
+     * <p>main.</p>
+     *
+     * @param args a {@link java.lang.String} object.
+     */
+    public static void main(final String... args) {
+        System.out.println("Hello World!");
     }
 }
