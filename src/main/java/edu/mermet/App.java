5f704feea3619d192b877607d193fd2e0b460630
Bruno Mermet <Bruno.Mermet@univ-lehavre.fr>
2016-05-07 23:43:07 +0200

    Ajout de pmd et findbugs

--- src/main/java/edu/mermet/App.java
+++ src/main/java/edu/mermet/App.java
@@ -12,6 +12,8 @@ public class App {
      *
      * @param args a {@link java.lang.String} object.
      */
+
+    public void methodeInutile() {}
     public static void main(final String... args) {
         System.out.println("Hello World!");
     }
